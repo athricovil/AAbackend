f4e88ba77b569e2eff8a9fd2f90ce2c5cb55da7c
aravind <athricovil@aadev>
2025-06-17 04:48:20 +0000

    users table using Hibernate

--- server/src/main/java/com/ayurayush/server/ServerApplication.java
+++ server/src/main/java/com/ayurayush/server/ServerApplication.java
@@ -1,22 +1,24 @@
 package com.ayurayush.server;
 
-import javax.crypto.SecretKey;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Encoders;
-import io.jsonwebtoken.security.Keys;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.ayurayush.server.entity")
+@EnableJpaRepositories("com.ayurayush.server.repository")
+@ComponentScan("com.ayurayush.server")
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-//		SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
-//		String base64Key = Encoders.BASE64.encode(key.getEncoded());
-//		System.out.println("Generated base64 key: " + base64Key);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
 
+        // Optional: Uncomment if you want to generate a JWT signing key
+        // SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        // String base64Key = Encoders.BASE64.encode(key.getEncoded());
+        // System.out.println("Generated base64 key: " + base64Key);
+    }
 }
+
